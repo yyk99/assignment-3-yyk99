6f47c1a24475e7ed6d07d004ab5f0ae9c7264f14
yyk <yyk@Ubuntu.myguest.virtualbox.org>
2026-01-16 20:15:12 -0500

    Update for Assignment 3 Part 2
    Get toolchain sysroot properly

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -98,12 +98,12 @@ ${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs (DONE)
 
-cp /opt/toolchains/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 \
-   ${OUTDIR}/rootfs/lib
-cp /opt/toolchains/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 \
-   /opt/toolchains/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 \
-   /opt/toolchains/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 \
-   ${OUTDIR}/rootfs/lib64
+echo "Toolchain info"
+aarch64-none-linux-gnu-gcc -print-sysroot -v
+
+TOOLCHAIN_SYSROOT=`aarch64-none-linux-gnu-gcc -print-sysroot`
+cp ${TOOLCHAIN_SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp ${TOOLCHAIN_SYSROOT}/lib64/{libm.so.6,libresolv.so.2,libc.so.6} ${OUTDIR}/rootfs/lib64
 
 # TODO: Make device nodes (DONE)
 
